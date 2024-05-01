4bfc7d80c7cf6786e29bb47ed4527046f2713e5c
Sheila Murugi <tmurugi49@gmail.com>
2024-05-01 19:38:28 +0300

    Transformations Using DBT first commit

---
+++ Dbt_workflows/models/example/fast_vehicles.sql
@@ -0,0 +1,27 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='view') }}
+
+with fast_vehicles as (
+    SELECT * 
+    from public."Vehicle_Information"
+
+    ORDER BY public."avg_speed" DESC
+    LIMIT 100
+)
+
+
+SELECT * FROM fast_vehicles
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
